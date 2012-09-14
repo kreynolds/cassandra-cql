2920713e71a65966c72f26426a90258f85c867b3
Kelley Reynolds <kelley@insidesystems.net>
2012-09-14 09:28:26 -0400

    Raise the connect_timeout and increase the default tested version to 1.1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 require 'rspec'
 
-CASSANDRA_VERSION = ENV['CASSANDRA_VERSION'] || '1.0' unless defined?(CASSANDRA_VERSION)
+CASSANDRA_VERSION = ENV['CASSANDRA_VERSION'] || '1.1' unless defined?(CASSANDRA_VERSION)
 
 $LOAD_PATH << "#{File.expand_path(File.dirname(__FILE__))}/../lib"
 require "cassandra-cql/#{CASSANDRA_VERSION}"
@@ -16,7 +16,7 @@ def yaml_fixture(file)
 end
 
 def setup_cassandra_connection
-  connection = CassandraCQL::Database.new(["127.0.0.1:9160"], {}, :retries => 2, :timeout => 1)
+  connection = CassandraCQL::Database.new(["127.0.0.1:9160"], {}, :retries => 5, :timeout => 1, :connect_timeout => 5)
   if !connection.keyspaces.map(&:name).include?("CassandraCQLTestKeyspace")
     connection.execute("CREATE KEYSPACE CassandraCQLTestKeyspace WITH strategy_class='org.apache.cassandra.locator.SimpleStrategy' AND strategy_options:replication_factor=1")
   end
